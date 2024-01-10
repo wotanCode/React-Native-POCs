37874653cf771f8ef42a1e0f4bd34bda5705211a
Pedro Yanez <72775296+wotanCode@users.noreply.github.com>
2024-01-10 10:16:25 -0300

    wip

---
+++ 01-calculator/src/screens/CalculatorScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function CalculatorScreen(): React.JSX.Element {
+  return (
+    <View>
+      <Text>Hola mundo</Text>
+    </View>
+  );
+}
+
+export default CalculatorScreen;
